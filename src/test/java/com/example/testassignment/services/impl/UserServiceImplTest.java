1646eeae45aeff8984b7803b4fd55795e4d6269b
FilosofDanil <serhio3347@gmail.com>
2024-04-28 22:30:27 +0200

    Added tests for invalid cases

--- src/test/java/com/example/testassignment/services/impl/UserServiceImplTest.java
+++ src/test/java/com/example/testassignment/services/impl/UserServiceImplTest.java
@@ -4,6 +4,9 @@ import com.example.testassignment.config.MinimalAgeConfiguration;
 import com.example.testassignment.dtos.UpdateUserDTO;
 import com.example.testassignment.dtos.UserDTO;
 import com.example.testassignment.entities.User;
+import com.example.testassignment.exceptions.InvalidDateException;
+import com.example.testassignment.exceptions.NoContentException;
+import com.example.testassignment.exceptions.ResourceNotFoundException;
 import com.example.testassignment.mappers.UserMapper;
 import com.example.testassignment.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
@@ -15,8 +18,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 
@@ -56,9 +59,9 @@ class UserServiceImplTest {
     @Test
     void createUser() {
         UserDTO userDTO = new UserDTO();
-        userDTO.setFirstName("John");
-        userDTO.setLastName("Doe");
-        userDTO.setEmail("john.doe@example.com");
+        userDTO.setFirstName("Danylo");
+        userDTO.setLastName("Berk");
+        userDTO.setEmail("Danylo.Berk.example.com");
         userDTO.setBirthDate(LocalDate.now().minusYears(25).toString());
         User user = User.builder()
                 .firstName(userDTO.getFirstName())
@@ -78,9 +81,9 @@ class UserServiceImplTest {
     void updateUser() {
         Long userId = 1L;
         UpdateUserDTO updateUserDTO = new UpdateUserDTO();
-        updateUserDTO.setFirstName("John");
-        updateUserDTO.setLastName("Doe");
-        updateUserDTO.setEmail("john.doe@example.com");
+        updateUserDTO.setFirstName("Danylo");
+        updateUserDTO.setLastName("Berk");
+        updateUserDTO.setEmail("Danylo.Berk.example.com");
         updateUserDTO.setBirthDate(LocalDate.now().minusYears(25).toString());
         when(userRepository.existsById(userId)).thenReturn(true);
         when(ageConfiguration.getMinimalAge()).thenReturn(18);
@@ -96,5 +99,54 @@ class UserServiceImplTest {
         when(userRepository.existsById(userId)).thenReturn(true);
         userService.deleteUser(userId);
         verify(userRepository, times(1)).deleteById(userId);
+    }
+
+
+    @Test
+    void createUserWithInvalidAge() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setFirstName("Danylo");
+        userDTO.setLastName("Berk");
+        userDTO.setEmail("Danylo.Berk.example.com");
+        userDTO.setBirthDate(LocalDate.now().minusYears(16).toString());
+
+        assertThrows(Exception.class, () -> userService.createUser(userDTO));
+        verify(userRepository, never()).save(any());
+    }
+
+    @Test
+    void createUserWithInvalidData() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setLastName("Berk");
+        userDTO.setEmail("Danylo.Berk.example.com");
+
+        assertThrows(Exception.class, () -> userService.createUser(userDTO));
+        verify(userRepository, never()).save(any());
+    }
+
+    @Test
+    void deleteUserNoContent() {
+        Long userId = 1L;
+
+        when(userRepository.existsById(userId)).thenReturn(false);
+
+        assertThrows(NoContentException.class, () -> userService.deleteUser(userId));
+        verify(userRepository, never()).deleteById(userId);
+    }
+
+    @Test
+    void updateUserNotFound() {
+        Long userId = 1L;
+        UpdateUserDTO updateUserDTO = new UpdateUserDTO();
+        updateUserDTO.setFirstName("Danylo");
+        updateUserDTO.setLastName("Berk");
+        updateUserDTO.setEmail("Danylo.Berk.example.com");
+        updateUserDTO.setBirthDate(LocalDate.now().minusYears(25).toString());
+
+        when(userRepository.existsById(userId)).thenReturn(false);
+
+        assertThrows(ResourceNotFoundException.class, ()
+                -> userService.updateUser(updateUserDTO, userId));
+        verify(userRepository, never()).save(any());
     }
 }
